c9bed0d4b1fe088e8f848e7cf7b72279a28ea8c2
Brett Reinhard <brett@reinhards.us>
2017-09-10 13:31:42 -0700

    Added overflow property to contain all passed content to the pages, any overflow with now be scrolled

--- src/booklet.test.js
+++ src/booklet.test.js
@@ -2,10 +2,24 @@ import expect from 'expect';
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import Booklet from './index';
-let props = {};
-function setup(bool) {
-    return shallow(<Booklet />);
+let props = {
+    index: 0,
+    flipPageIndex: 0,
+    pages: [<div />, <div />, <div />, <div />, <div />],
+};
+function setup() {
+    return shallow(<Booklet {...props} />);
 }
 describe('Test Booklet Mounts ', () => {
-    it('', () => {});
+    it('Test Container ClassName', () => {
+        const wrapper = setup();
+        expect(wrapper.node.props.className).toBe('book booklet-container');
+    });
+    it('Test Container Children ClassNames', () => {
+        const wrapper = setup();
+        expect(wrapper.node.props.children[0].props.className).toBe('pagelast booklet-next-right');
+        expect(wrapper.node.props.children[1].props.className).toBe('page booklet-next-left');
+        expect(wrapper.node.props.children[2].props.className).toBe('page3 booklet-current-right');
+        expect(wrapper.node.props.children[3].props.className).toBe('cover');
+    });
 });
